5ce2b14307accc6d3d6fdb3cc429cfd78e3bc80a
Angelina Ruiz <125549217+angeruiizz@users.noreply.github.com>
2025-01-11 19:08:01 +0100

    Añadir HUD(barra vida, barra boost, vidas generales, bomba)

--- Assets/Scripts/Vida.cs
+++ Assets/Scripts/Vida.cs
@@ -1,15 +1,19 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Vida : MonoBehaviour
 {
 	[SerializeField]
 	float maxHealth = 100.0f;
 	float currentHealth;
+	public Image BarraVida; // Referencia a la barra de vida (UI)
 	
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         currentHealth = maxHealth;
+		if (BarraVida != null)
+            BarraVida.fillAmount = 1f; // Barra llena al inicio
     }
 
     // Update is called once per frame
@@ -22,6 +26,10 @@ public class Vida : MonoBehaviour
 	
 		currentHealth -= amount;
 		currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
+
+		// Actualizar la barra de vida
+        if (BarraVida != null)
+            BarraVida.fillAmount = currentHealth / maxHealth;
 
 		if(currentHealth == 0){
 			Destroy(gameObject);
