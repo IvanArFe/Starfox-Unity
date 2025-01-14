6dd7c7ed657df87de2736e52255e91349c2b4932
Miguel Lopes <pilouco2602@gmail.com>
2025-01-14 12:39:24 +0100

    kills

--- Assets/Scripts/randomStars.cs
+++ Assets/Scripts/randomStars.cs
@@ -8,8 +8,14 @@ public class randomStars : MonoBehaviour
     public GameObject stars;
     private Vector3 randomVector;
 
+    [SerializeField] 
+    public int range_bottom_y = 100;
+    public int range_bottom_x = -200;
+    public int range_top_x = 100;
+    public int range_top_y = 200;
+
     public void Update(){
-        randomVector = new Vector3(Random.Range(-100,100),Random.Range(-15,-3), 35);
+        randomVector = new Vector3(Random.Range(range_bottom_x,range_top_x),Random.Range(range_bottom_y,range_top_y), 35);
         Instantiate(stars, randomVector, transform.rotation);
 
     }
